73d7abc0c9df6c95a3a7dd11f5a41e2bc1affb3e
Megan Chan <meganychan@gmail.com>
2015-09-29 11:57:54 -0500

    "Changed the HTML for the site"

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
